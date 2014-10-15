1bc12e43251b709f137673eabf37dd5ca02995b7
nflavour <nflavour@gmail.com>
2014-10-14 21:31:58 -0400

    added bourbon and changed to sass files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root "users#index", :via => [:get, :post]
-
+  resources :users
   match ':controller(/:action(/:id))',
     :via => [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
